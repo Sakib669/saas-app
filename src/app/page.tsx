1076d88b7b44ea3f721e86d9084f877b70fe7dbd
Shafiqul Islam Sakib <stewieboyyy@gmail.com>
2026-03-16 00:46:53 +0600

    initialized the app with next.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <div>
+      <h1 className="text-2xl underline">
+        Welcome to the Saas App
+        <Button className=''>
+            Let's get started
+        </Button>
+      </h1>
+    </div>
   )
 }
 
